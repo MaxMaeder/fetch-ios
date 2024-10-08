9d7bd246d45d66228549338cf601d1833be9f8e4
Max Maeder <max.s.maeder@gmail.com>
2024-10-08 11:23:59 -0500

    Initial Commit

---
+++ fetch-ios/fetch_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  fetch_iosApp.swift
+//  fetch-ios
+//
+//  Created by Max Maeder on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct fetch_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
